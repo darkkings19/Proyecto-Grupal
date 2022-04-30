2827c9f8714dc690d8e9292ecd073dc8e68efbb2
Paolo Paredes <76070438+darkkings19@users.noreply.github.com>
2022-04-30 16:42:52 -0400

    Add files via upload

---
+++ ufropicture/src/ufropicture.java
@@ -0,0 +1,28 @@
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class ufropicture {
+    public static void main(String[] args) throws IOException {
+
+        File file = new File("C:\\Users\\polot\\Downloads\\ufropicture\\prueba.jpg");
+        BufferedImage bufferedImage = ImageIO.read(file);
+
+        ImageIcon imageIcon = new ImageIcon(bufferedImage);
+        JFrame jFrame = new JFrame();
+
+        jFrame.setLayout(new FlowLayout());
+
+        jFrame.setSize(500, 500);
+        JLabel jLabel = new JLabel();
+
+        jLabel.setIcon(imageIcon);
+        jFrame.add(jLabel);
+        jFrame.setVisible(true);
+
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
